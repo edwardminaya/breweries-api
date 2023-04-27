3633e0d6c80410adb65c16031113bb8409d2a7ae
mikemccomb <mikemccomb@gmail.com>
2023-04-27 12:57:20 -0500

    Renames column 'type' to 'brewery_type'

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_172149) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_175525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "breweries", force: :cascade do |t|
     t.string "brewery_name"
-    t.string "type"
+    t.string "brewery_type"
     t.string "address"
     t.string "website"
     t.string "state"
