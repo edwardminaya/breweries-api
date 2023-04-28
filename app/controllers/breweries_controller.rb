5aa6b6cc251becc3109b53c9653ab40c156b500f
“Edward <eminaya49@gmail.com>
2023-04-28 11:15:38 -0500

    added state param to index

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,6 +1,11 @@
 class BreweriesController < ApplicationController
   def index
-    @breweries = Brewery.all
-    render :index
+    if params[:state]
+      @breweries = Brewery.where(state: params[:state])
+      render :index
+    else
+      @breweries = Brewery.all
+      render :index
+    end
   end
 end
